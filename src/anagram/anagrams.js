ce68bed2c28ba9058b47c799e9721f6fd61ca642
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-19 14:55:24 +0100

    Format documents

--- src/anagram/anagrams.js
+++ src/anagram/anagrams.js
@@ -1,7 +1,12 @@
-
 function filterLettersWithoutSpaces(string) {
-    return string.split('').filter(letter=>letter!==' ').sort().join('');
+  return string
+    .split('')
+    .filter((letter) => letter !== ' ')
+    .sort()
+    .join('');
 }
-export function isAnagram(string1,string2) {
-    return filterLettersWithoutSpaces(string1) === filterLettersWithoutSpaces(string2);
+export function isAnagram(string1, string2) {
+  return (
+    filterLettersWithoutSpaces(string1) === filterLettersWithoutSpaces(string2)
+  );
 }
--- src/anagram/anagrams.test.js
+++ src/anagram/anagrams.test.js
@@ -1,15 +1,15 @@
-import {isAnagram} from './anagrams';
+import { isAnagram } from './anagrams';
 describe('isAnagram function :', () => {
-    it('should return true when the two strings are anagrams', () => {
-        expect(isAnagram('listen', 'silent')).toBe(true);
-    });
-   
-    it('should return false when the two strings have same letter but different length', () => {
-        expect(isAnagram('listen', 'silentt')).toBe(false);
-    });
-
-    it('should return true when the arguments have spaces', () => {
-        expect(isAnagram('conversation', 'voices rant on')).toBe(true);
-    });
+  it('should return true when the two strings are anagrams', () => {
+    expect(isAnagram('listen', 'silent')).toBe(true);
   });
-  
+
+  it('should return false when the two strings have same letter but different length', () => {
+    expect(isAnagram('listen', 'silentt')).toBe(false);
+  });
+
+  it('should return true when the arguments have spaces', () => {
+    expect(isAnagram('conversation', 'voices rant on')).toBe(true);
+  });
+
+});
