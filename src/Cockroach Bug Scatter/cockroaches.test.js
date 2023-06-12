3f4e83c03f84779a4e56eb6ee485f68c97798a85
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-12 09:59:30 +0100

    Add test for holes position

--- src/Cockroach Bug Scatter/cockroaches.test.js
+++ src/Cockroach Bug Scatter/cockroaches.test.js
@@ -1,18 +1,22 @@
-import { findAllCockroaches, moveCockroachToWall } from "./cockroaches";
+import {
+  findAllCockroaches,
+  moveCockroachToWall,
+  findAllHolesFromTopToLeft,
+} from "./cockroaches";
 
 const room1 = [
-  "+----------------0---------------+",
+  "8----6-----------0---------------+",
   "|                                |",
   "|                                |",
-  "|          U        D            |",
-  "|     L                          |",
+  "7          U        D            |",
+  "|     L                          9",
   "|              R                 |",
   "|           L                    |",
   "|  U                             1",
   "3        U    D                  |",
   "|         L              R       |",
   "|                                |",
-  "+----------------2---------------+",
+  "+---------------2---------------+",
 ];
 
 const room1Cockroaches = [
@@ -26,6 +30,17 @@ const room1Cockroaches = [
   { position: [8, 14], direction: "D" },
   { position: [9, 10], direction: "L" },
   { position: [9, 25], direction: "R" },
+];
+
+const holesFromTopToLeft = [
+  { position: [0, 17], holeIndex: "0" },
+  { position: [0, 5], holeIndex: "6" },
+  { position: [0, 0], holeIndex: "8" },
+  { position: [3, 0], holeIndex: "7" },
+  { position: [8, 0], holeIndex: "3" },
+  { position: [11, 16], holeIndex: "2" },
+  { position: [7, 33], holeIndex: "1" },
+  { position: [4, 33], holeIndex: "9" },
 ];
 
 describe("findAllCockroaches function :", () => {
@@ -52,5 +67,11 @@ describe("moveCockroachToWall function :", () => {
       position: [5, 33],
       direction: "R",
     });
+  });
+});
+
+describe("findAllHolesFromTopToLeft function :", () => {
+  it("should return all holes positions starting from top and going to left", () => {
+    expect(findAllHolesFromTopToLeft(room1)).toEqual(holesFromTopToLeft);
   });
 });
