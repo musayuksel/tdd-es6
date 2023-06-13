134d1a43ab94ae7d2f26a9b7686c91d3c70c8899
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-13 12:11:48 +0100

    Add test and code for findDiff function

---
+++ src/Find the missing term in an Arithmetic Progression/findMissing.js
@@ -0,0 +1,6 @@
+const findDiff = (numbers) =>
+  (numbers[numbers.length - 1] - numbers[0]) / numbers.length;
+
+const findMissing = (numbers) => {};
+
+export { findMissing, findDiff };
---
+++ src/Find the missing term in an Arithmetic Progression/findMissing.test.js
@@ -0,0 +1,11 @@
+import { findMissing, findDiff } from "./findMissing.js";
+
+describe("findDiff function", () => {
+  it("should return difference between first and last number divided by length of array", () => {
+    const mockNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+    expect(findDiff(mockNumbers)).toEqual(1);
+
+    const mockNumbers2 = [1, 3, 5, 9, 11];
+    expect(findDiff(mockNumbers2)).toEqual(2);
+  });
+});
