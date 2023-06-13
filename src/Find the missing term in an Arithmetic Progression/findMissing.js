83e47599150a8b64050f8acd523e3bda2b4be22b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-13 12:13:47 +0100

    Add test and code for findMissing function

--- src/Find the missing term in an Arithmetic Progression/findMissing.js
+++ src/Find the missing term in an Arithmetic Progression/findMissing.js
@@ -1,6 +1,14 @@
 const findDiff = (numbers) =>
   (numbers[numbers.length - 1] - numbers[0]) / numbers.length;
 
-const findMissing = (numbers) => {};
+const findMissing = (numbers) => {
+  const diff = findDiff(numbers);
+
+  const missingNumber = numbers.find(
+    (number, index) => numbers[index + 1] - number !== diff
+  );
+
+  return missingNumber + diff;
+};
 
 export { findMissing, findDiff };
--- src/Find the missing term in an Arithmetic Progression/findMissing.test.js
+++ src/Find the missing term in an Arithmetic Progression/findMissing.test.js
@@ -9,3 +9,13 @@ describe("findDiff function", () => {
     expect(findDiff(mockNumbers2)).toEqual(2);
   });
 });
+
+describe("findMissing function", () => {
+  it("should return missing number", () => {
+    const mockNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+    expect(findMissing(mockNumbers)).toEqual(9);
+
+    const mockNumbers2 = [1, 3, 5, 9, 11];
+    expect(findMissing(mockNumbers2)).toEqual(7);
+  });
+});
