d26f253cc30f096be3e03fde276cea9457a9bb56
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-14 12:30:15 +0100

    Add test and code for pyramid function

--- src/ Pyramid Array/pyramidArray.js
+++ src/ Pyramid Array/pyramidArray.js
@@ -1,2 +1,6 @@
 const createSubArray = (number) => Array.from({ length: number }, () => 1);
-export { createSubArray };
+
+const pyramid = (number) =>
+  Array.from({ length: number }, (_, index) => createSubArray(index + 1));
+
+export { createSubArray, pyramid };
--- src/ Pyramid Array/pyramidArray.test.js
+++ src/ Pyramid Array/pyramidArray.test.js
@@ -1,8 +1,21 @@
-import { createSubArray } from "./pyramidArray";
+import { createSubArray, pyramid } from "./pyramidArray";
 
 describe("createSubArray function", () => {
   it("should return array with number of length and filled with ones", () => {
-    const mockNumber = 4;
+    let mockNumber = 1;
+    expect(createSubArray(mockNumber)).toEqual([1]);
+
+    mockNumber = 4;
     expect(createSubArray(mockNumber)).toEqual([1, 1, 1, 1]);
+  });
+});
+
+describe("pyramid function", () => {
+  it("should return pyramid array", () => {
+    let mockNumber = 1;
+    expect(pyramid(mockNumber)).toEqual([[1]]);
+
+    mockNumber = 4;
+    expect(pyramid(mockNumber)).toEqual([[1], [1, 1], [1, 1, 1], [1, 1, 1, 1]]);
   });
 });
