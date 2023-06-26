1699a54303b09c667ca4512617ccbe0f34852f36
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-26 15:38:25 +0100

    Add more test

--- src/xD-Arrays for dimmies/dim.test.js
+++ src/xD-Arrays for dimmies/dim.test.js
@@ -4,4 +4,15 @@ describe("createArrayFromValue function", () => {
   it("should return an array of 3 elements with the value 0", () => {
     expect(createArrayFromValue(3, 0)).toEqual([0, 0, 0]);
   });
+
+  it("should call the function if the value is a function", () => {
+    const fn = jest.fn();
+    createArrayFromValue(3, fn);
+    expect(fn).toHaveBeenCalledTimes(3);
+  });
+
+  it("should add the return value of the function to the array", () => {
+    const fn = jest.fn(() => 5);
+    expect(createArrayFromValue(3, fn)).toEqual([5, 5, 5]);
+  });
 });
