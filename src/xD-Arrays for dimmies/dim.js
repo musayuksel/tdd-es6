9b7a2e9c2e166da37a9a605308d3330a1ed2ce59
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-07 16:07:19 +0100

    Delete console

--- src/xD-Arrays for dimmies/dim.js
+++ src/xD-Arrays for dimmies/dim.js
@@ -1,6 +1,5 @@
 function dim(...args) {
   let value = args.pop();
-  console.log({ args, value });
   while (args.length > 0) {
     const currentArg = args.pop();
     value = createArrayFromValue(currentArg, value);
@@ -10,7 +9,7 @@ function dim(...args) {
 }
 
 function createArrayFromValue(number, value) {
-  if (typeof value === "function") {
+  if (typeof value === 'function') {
     //fill the array wih the value of the function
     return new Array(number).fill(0).map(value);
   }
