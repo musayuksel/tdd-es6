44bd58af02ccc7a8f8d7c29b4e19e694128143e5
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-26 13:15:48 +0100

    Add test and code for bouncingBall function

--- src/Bouncing Balls/bouncingBall.js
+++ src/Bouncing Balls/bouncingBall.js
@@ -1,4 +1,16 @@
 const isInputValid = (h, bounce, window) =>
   h > 0 && bounce > 0 && bounce < 1 && window < h;
 
-export { isInputValid };
+const bouncingBall = (h, bounce, window) => {
+  if (!isInputValid(h, bounce, window)) return -1;
+
+  let bounceCount = 1;
+
+  while (h * bounce > window) {
+    bounceCount += 2;
+    h *= bounce;
+  }
+  return bounceCount;
+};
+
+export { isInputValid, bouncingBall };
--- src/Bouncing Balls/bouncingBall.test.js
+++ src/Bouncing Balls/bouncingBall.test.js
@@ -1,4 +1,4 @@
-import { isInputValid } from './bouncingBall';
+import { isInputValid, bouncingBall } from './bouncingBall';
 
 describe('isInputValid', () => {
   it('should return true if input is valid', () => {
@@ -15,5 +15,15 @@ describe('isInputValid', () => {
 
   it('should return false if window is invalid', () => {
     expect(isInputValid(3, 0.66, 3.5)).toBe(false);
+  });
+});
+
+describe('bouncingBall', () => {
+  it('should return -1 if input is invalid', () => {
+    expect(bouncingBall(-3, 0.66, 1.5)).toBe(-1);
+  });
+
+  it('h = 3, bounce = 0.66, window = 1.5 should return ', () => {
+    expect(bouncingBall(3, 0.66, 1.5)).toBe(3);
   });
 });
